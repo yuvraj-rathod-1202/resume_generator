8dbf83170dae0db468a38d04d9e3dd14c87d7734
yuvraj-rathod-1202 <rathodyuvraj12022007>
2024-11-05 22:55:36 +0530

    add instruction for ai section

--- groq-script.js
+++ groq-script.js
@@ -7,27 +7,33 @@ const MODEL = 'llama-3.1-8b-instant'
 async function enhanceDescription(section) {
 	let element1
 	let element2
+	let instruction
 	switch(section){
 		case 'internship':
-			element1 = document.getElementById("interndescriptionF")
-			element2 = document.getElementById("interndescription2F")
+			element1 = document.getElementById("interndescriptionF");
+			element2 = document.getElementById("interndescription2F");
+			instruction = document.getElementById("internMessageToAI");
 			break;
 		case 'project':
-			element1 = document.getElementById("projectdescriptionF")
-			element2 = document.getElementById("projectdescription2F")
+			element1 = document.getElementById("projectdescriptionF");
+			element2 = document.getElementById("projectdescription2F");
+			instruction = document.getElementById("projectMessageToAI");
 			break;
 		case 'por':
 			element1 = document.getElementById("PORdescriptionF");
 			element2 = document.getElementById("PORdescription2F");
+			instruction = document.getElementById("porMessageToAI");
 			break;
 		case 'skill':
 			element1 = document.getElementById("skilldescriptionF");
 			element2 = document.getElementById("skilldescriptionF");
+			instruction = document.getElementById("skillMessageToAI");
 			break;
 	}
 
 	const description = element1.value;
 	const description2 = element2.value;
+	const instructionforAI = instruction.value;
 
 	// handle empty fields
 	if(description === '' || description2 === '') {
@@ -36,10 +42,11 @@ async function enhanceDescription(section) {
 	}
 
 	// set placeholder as 'enhancing'
-	element1.value = ""
-	element2.value = ""
-	element1.placeholder = "Enhancing Text..."
-	element2.placeholder = "Enhancing Text..."
+	element1.value = "";
+	element2.value = "";
+	instruction.value = "";
+	element1.placeholder = "Enhancing Text...";
+	element2.placeholder = "Enhancing Text...";
 
 	// console.log(description, description2)
 	const response = await fetch('https://api.groq.com/openai/v1/chat/completions', {
@@ -53,7 +60,7 @@ async function enhanceDescription(section) {
                     {
                         role: 'system',
                         content:
-                            `-You are a text enhancer assistant. -You are given 2 lines of description about an ${section}, enhance them for them to be put in a professional resume, highlighting key points and useful skills involved in the ${section}. -Do this within 2 short lines only for EACH input line. -Return only the enhanced texts, separated by a line, without any prefixes like 'Enhanced text:'.`
+                            `-You are a text enhancer assistant. -You are given 2 lines of description and one instruction from user that is ${section}, enhance them for them to be put in a professional resume, highlighting key points and useful skills involved in the ${section}. -Do this within 2 short lines only for EACH input line and make changes as users instruct you. -Return only the enhanced texts, separated by a line, without any prefixes like 'Enhanced text:'.`
 
                     },
                     {
